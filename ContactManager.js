72e7e38e09422e72cef2fc0f2b3a7991d6f84569
Adedeji Jola <adedejijolaoluwa@gmail.com>
2018-05-29 18:46:27 +0100

    Added Sticky Position

--- ContactManager.js
+++ ContactManager.js
@@ -66,7 +66,6 @@ class ContactManager{
 
     static listContacts(){
         document.querySelector("#contacts").innerHTML = "";
-        
         ContactManager.loadLocalStorage();
     }
     static sortContacts(){
